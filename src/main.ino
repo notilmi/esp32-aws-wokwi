bd827ddd243438d3c9171959b1569485c3ac5606
Achmad Ilmi Al Akbar <113961961+notilmi@users.noreply.github.com>
2024-07-12 21:11:01 +0700

    Fix: AWS Connection

--- src/main.ino
+++ src/main.ino
@@ -2,126 +2,115 @@
 #include <WiFiClientSecure.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
-#include <WiFi.h>
-#include <NTPClient.h>
-#include <DHT.h>
-#include <Wire.h>
-#define DHTPIN 15     // Digital pin connected to the DHT sensor
+#include "WiFi.h"
+ 
+#include "DHT.h"
+#define DHTPIN 15    // Digital pin connected to the DHT sensor
 #define DHTTYPE DHT22   // DHT 11
-
+ 
 #define AWS_IOT_PUBLISH_TOPIC   "esp32/pub"
 #define AWS_IOT_SUBSCRIBE_TOPIC "esp32/sub"
-
+ 
 float h;
 float t;
 
-void messageHandler(char* topic, byte* payload, unsigned int length)
-{
-  Serial.print("Incoming message from topic: ");
-  Serial.println(topic);
-
-  StaticJsonDocument<200> doc;
-  deserializeJson(doc, payload, length);
-  const char* message = doc["message"];
-  Serial.println(message);
-}
-
 DHT dht(DHTPIN, DHTTYPE);
-
-WiFiClientSecure net;
+ 
+WiFiClientSecure net = WiFiClientSecure();
 PubSubClient client(net);
-WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "id.pool.ntp.org");
-
+ 
 void connectAWS()
 {
   WiFi.mode(WIFI_STA);
-  WiFi.begin("Wokwi-GUEST", "");
-
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+ 
   Serial.println("Connecting to Wi-Fi");
-
+ 
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
-    Serial.print("Not Connected");
+    Serial.print(".");
   }
-
+ 
   // Configure WiFiClientSecure to use the AWS IoT device credentials
   net.setCACert(AWS_CERT_CA);
   net.setCertificate(AWS_CERT_CRT);
   net.setPrivateKey(AWS_CERT_PRIVATE);
-
+ 
   // Connect to the MQTT broker on the AWS endpoint we defined earlier
   client.setServer(AWS_IOT_ENDPOINT, 8883);
-
+ 
   // Create a message handler
   client.setCallback(messageHandler);
-
-  Serial.println("Connecting to AWS IoT");
-
-  while (!client.connected())
+ 
+  Serial.println("Connecting to AWS IOT");
+ 
+  while (!client.connect(THINGNAME))
   {
     Serial.print(".");
-    if (client.connect(THINGNAME))
-    {
-      Serial.println("Connected to AWS IoT");
-      client.subscribe(AWS_IOT_SUBSCRIBE_TOPIC);
-    }
-    else
-    {
-      Serial.println("AWS IoT Connection Failed! Retrying...");
-      delay(1000);
-    }
+    delay(100);
   }
+ 
+  if (!client.connected())
+  {
+    Serial.println("AWS IoT Timeout!");
+    return;
+  }
+ 
+  // Subscribe to a topic
+  client.subscribe(AWS_IOT_SUBSCRIBE_TOPIC);
+ 
+  Serial.println("AWS IoT Connected!");
 }
-
+ 
 void publishMessage()
 {
   StaticJsonDocument<200> doc;
-  doc["timestamp"] = timeClient.getEpochTime() + (7 * 3600);  
   doc["humidity"] = h;
   doc["temperature"] = t;
   char jsonBuffer[512];
-
   serializeJson(doc, jsonBuffer); // print to client
-
+ 
   client.publish(AWS_IOT_PUBLISH_TOPIC, jsonBuffer);
 }
-
+ 
+void messageHandler(char* topic, byte* payload, unsigned int length)
+{
+  Serial.print("incoming: ");
+  Serial.println(topic);
+ 
+  StaticJsonDocument<200> doc;
+  deserializeJson(doc, payload);
+  const char* message = doc["message"];
+  Serial.println(message);
+}
+ 
 void setup()
 {
   Serial.begin(115200);
   connectAWS();
   dht.begin();
-  Wire.begin();
-  timeClient.begin();
-  timeClient.setTimeOffset(0);
 }
+ 
 void loop()
 {
   h = dht.readHumidity();
   t = dht.readTemperature();
-
-  timeClient.update();
-  if (isnan(h) || isnan(t))
+ 
+ 
+  if (isnan(h) || isnan(t) )  // Check if any reads failed and exit early (to try again).
   {
     Serial.println(F("Failed to read from DHT sensor!"));
     return;
   }
-
+ 
   Serial.print(F("Humidity: "));
   Serial.print(h);
   Serial.print(F("%  Temperature: "));
   Serial.print(t);
   Serial.println(F("°C "));
-
-  if (WiFi.status() == WL_CONNECTED) {
-    Serial.println("WiFi Connected");
-  } else {
-    Serial.println("WiFi Not Connected");
-  }
-
+ 
   publishMessage();
   client.loop();
-  delay(5000);
+  delay(1000);
 }
